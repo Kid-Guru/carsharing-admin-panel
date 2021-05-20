df348a99e080b3c5fa7d942dd7b8154033d9e059
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-20 12:25:01 +0400

    add Formik to login form

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,3 +1,4 @@
+import { Form, Formik } from 'formik';
 import { useEffect, useState } from 'react';
 import { CSSTransition } from 'react-transition-group';
 import Box from '../common/Box/Box';
@@ -26,15 +27,25 @@ function LoginPage() {
       <div className={s.login__wrapper}>
         <h1 className={s.title}>Need for drive</h1>
         <Box>
-          <form className={s.login__form} action="submit">
-            <h2 className={s.login__title}>Вход</h2>
-            <FormInputText label="Почта" placeholder="Введите почту" type="email" />
-            <FormInputText label="Пароль" placeholder="Введите пароль" type="password" />
-            <p className={s.login__footer}>
-              <Link text="Запросить доступ" href="https://gist.github.com/internship-simbirsoft/3d634f32072b1e8514fc9fb19f88acdd" />
-              <ButtonSubmit text="Войти" />
-            </p>
-          </form>
+          <Formik
+            initialValues={{
+              login: '',
+              password: '',
+            }}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            <Form className={s.login__form} action="submit">
+              <h2 className={s.login__title}>Вход</h2>
+              <FormInputText label="Почта" placeholder="Введите почту" type="email" name="login" />
+              <FormInputText label="Пароль" placeholder="Введите пароль" type="password" name="password" />
+              <p className={s.login__footer}>
+                <Link text="Запросить доступ" href="https://gist.github.com/internship-simbirsoft/3d634f32072b1e8514fc9fb19f88acdd" />
+                <ButtonSubmit text="Войти" />
+              </p>
+            </Form>
+          </Formik>
         </Box>
       </div>
     </CSSTransition>
--- src/components/common/FormInputText/FormInputText.jsx
+++ src/components/common/FormInputText/FormInputText.jsx
@@ -1,12 +1,18 @@
+import { Field } from 'formik';
 import s from './FormInputText.module.scss';
 
 function FormInputText(props) {
-  const { label = 'label', placeholder, type = 'text' } = props;
+  const {
+    label = 'label',
+    placeholder,
+    type = 'text',
+    name,
+  } = props;
   return (
     <div className={s.root}>
       <label className={s.label} htmlFor={label}>
         <span className={s.title}>{label}</span>
-        <input className={s.input} id={label} placeholder={placeholder} type={type} />
+        <Field className={s.input} name={name} id={label} placeholder={placeholder} type={type} />
       </label>
     </div>
   );
