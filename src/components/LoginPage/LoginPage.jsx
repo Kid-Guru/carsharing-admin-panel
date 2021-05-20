f2a874e6c1040e4f7d7ff28b56d923b89c5b96d6
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-20 11:44:08 +0400

    add transition appear LoginPage

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,25 +1,43 @@
+import { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import Box from '../common/Box/Box';
+import ButtonSubmit from '../common/ButtonSubmit/ButtonSubmit';
 import FormInputText from '../common/FormInputText/FormInputText';
 import Link from '../common/Link/Link';
-import ButtonSubmit from '../common/ButtonSubmit/ButtonSubmit';
 import s from './LoginPage.module.scss';
 
 function LoginPage() {
+  const [isShowing, setShowing] = useState(false);
+  useEffect(() => {
+    setShowing(true);
+  }, []);
   return (
-    <div className={s.login__wrapper}>
-      <h1 className={s.title}>Need for drive</h1>
-      <Box>
-        <form className={s.login__form} action="submit">
-          <h2 className={s.login__title}>Вход</h2>
-          <FormInputText label="Почта" placeholder="Введите почту" type="email" />
-          <FormInputText label="Пароль" placeholder="Введите пароль" type="password" />
-          <p className={s.login__footer}>
-            <Link text="Запросить доступ" href="https://gist.github.com/internship-simbirsoft/3d634f32072b1e8514fc9fb19f88acdd" />
-            <ButtonSubmit text="Войти" />
-          </p>
-        </form>
-      </Box>
-    </div>
+    <CSSTransition
+      in={isShowing}
+      timeout={300}
+      classNames={{
+        enter: s.transitionEnter,
+        enterActive: s.transitionEnterActive,
+        // exitActive: s.transitionExitActive,
+        // exit: s.transitionExit,
+      }}
+      unmountOnExit
+    >
+      <div className={s.login__wrapper}>
+        <h1 className={s.title}>Need for drive</h1>
+        <Box>
+          <form className={s.login__form} action="submit">
+            <h2 className={s.login__title}>Вход</h2>
+            <FormInputText label="Почта" placeholder="Введите почту" type="email" />
+            <FormInputText label="Пароль" placeholder="Введите пароль" type="password" />
+            <p className={s.login__footer}>
+              <Link text="Запросить доступ" href="https://gist.github.com/internship-simbirsoft/3d634f32072b1e8514fc9fb19f88acdd" />
+              <ButtonSubmit text="Войти" />
+            </p>
+          </form>
+        </Box>
+      </div>
+    </CSSTransition>
   );
 }
 
