c00e6f7be7d36c4bd1fb403a251a37028cd29d95
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-21 12:46:28 +0400

    fix adaptive login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -14,30 +14,32 @@ const validationSchema = yup.object().shape({
 function Login(props) {
   const { onSubmitHandle } = props;
   return (
-    <div className={s.login__wrapper}>
-      <h1 className={s.title}>Need for drive</h1>
-      <Box>
-        <Formik
-          initialValues={{
-            login: '',
-            password: '',
-          }}
-          validationSchema={validationSchema}
-          onSubmit={onSubmitHandle}
-        >
-          {({ touched, errors, isSubmitting }) => (
-            <Form className={s.login__form} action="submit">
-              <h2 className={s.login__title}>Вход</h2>
-              <FormInputText label="Почта" placeholder="Введите почту" type="text" name="login" isError={touched.login && errors.login} errorText={errors.login} />
-              <FormInputText label="Пароль" placeholder="Введите пароль" type="password" name="password" isError={touched.password && errors.password} errorText={errors.password} />
-              <p className={s.login__footer}>
-                <Link text="Запросить доступ" href="https://gist.github.com/internship-simbirsoft/3d634f32072b1e8514fc9fb19f88acdd" />
-                <ButtonSubmit text="Войти" disabled={isSubmitting} />
-              </p>
-            </Form>
-          )}
-        </Formik>
-      </Box>
+    <div className={s.login__outer}>
+      <div className={s.login__wrapper}>
+        <h1 className={s.title}>Need for drive</h1>
+        <Box>
+          <Formik
+            initialValues={{
+              login: '',
+              password: '',
+            }}
+            validationSchema={validationSchema}
+            onSubmit={onSubmitHandle}
+          >
+            {({ touched, errors, isSubmitting }) => (
+              <Form className={s.login__form} action="submit">
+                <h2 className={s.login__title}>Вход</h2>
+                <FormInputText label="Почта" placeholder="Введите почту" type="text" name="login" isError={touched.login && errors.login} errorText={errors.login} />
+                <FormInputText label="Пароль" placeholder="Введите пароль" type="password" name="password" isError={touched.password && errors.password} errorText={errors.password} />
+                <p className={s.login__footer}>
+                  <Link text="Запросить доступ" href="https://gist.github.com/internship-simbirsoft/3d634f32072b1e8514fc9fb19f88acdd" />
+                  <ButtonSubmit text="Войти" disabled={isSubmitting} />
+                </p>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </div>
     </div>
   );
 }
