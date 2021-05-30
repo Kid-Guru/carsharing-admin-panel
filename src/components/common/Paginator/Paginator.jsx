04cf1ddabfdde53b2fe334cff4497e6e377493eb
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-30 16:42:18 +0400

    fix styles and add necessary props

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,19 +1,21 @@
 import ReactPaginate from 'react-paginate';
 import s from './Paginator.module.scss';
 
-function Paginator() {
-  // const {
-  //   options, form, field, placeholder,
-  // } = props;
+function Paginator(props) {
   return (
     <div className={s.wrapper}>
       <ReactPaginate
+        {...props}
         containerClassName={s.pagination}
         pageClassName={s.item}
         breakClassName={s.item}
         previousClassName={s.item}
         nextClassName={s.item}
         activeClassName={s.item__active}
+        pageLinkClassName={s.item__link}
+        breakLinkClassName={s.item__break}
+        previousLinkClassName={s.item__side}
+        nextLinkClassName={s.item__side}
         previousLabel="«"
         nextLabel="»"
       />
