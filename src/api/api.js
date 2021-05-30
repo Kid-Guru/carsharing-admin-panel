80d1f8fbee2464f452940b644d7005fa89c267a4
Kid-Guru <birkov.alexandr@gmail.com>
2021-05-30 16:44:05 +0400

    some fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
+import queryString from 'query-string';
+import cookieHelper from '../helpers/cookieHelper';
 
 const APPLICATION_ID = process.env.REACT_APP_X_API_APPLICATION_ID;
 const SECRET = process.env.REACT_APP_X_API_SECRET;
 
-export const URL = 'https://api-factory.simbirsoft1.com';
-export const API_URL = 'https://api-factory.simbirsoft1.com/api';
-export const AUTH_URL = {
-  login: '/auth/login/oauth',
+const URL = 'https://api-factory.simbirsoft1.com';
+const API_URL = 'https://api-factory.simbirsoft1.com/api';
+const AUTH_URL = {
+  // login: '/auth/login/oauth',
+  login: '/auth/login',
 };
 
-export const authInstance = axios.create({
+const authInstance = axios.create({
   baseURL: API_URL,
   headers: {
     'X-Api-Factory-Application-Id': APPLICATION_ID,
@@ -29,41 +32,59 @@ authInstance.interceptors.request.use(
 
 const authLogin = (requestBody) => authInstance.post(AUTH_URL.login, requestBody);
 
-export const APIInstance = axios.create({
+
+
+
+const mainInstance = axios.create({
   baseURL: API_URL,
   headers: {
     'X-Api-Factory-Application-Id': APPLICATION_ID,
   },
 });
 
-const orderApi = {
-  getCity() {
-    return APIInstance.get('db/city/');
+mainInstance.interceptors.request.use(
+  (config) => ({
+    ...config,
+    data: {
+      ...config.data,
+      client_secret: SECRET,
+    },
+    headers: {
+      ...config.headers,
+      authorization: `Bearer ${cookieHelper.getAccessToken()}`,
+    },
+  }),
+);
+
+const api = {
+  getOrders(params) {
+    const stringified = queryString.stringify(params);
+    return mainInstance.get(`db/order?${stringified}`);
   },
-  getPoint() {
-    return APIInstance.get('db/point/');
-  },
-  getCars() {
-    return APIInstance.get('db/car/');
-  },
-  getCategories() {
-    return APIInstance.get('db/category/');
-  },
-  getRates() {
-    return APIInstance.get('db/rate');
-  },
-  getStatuses() {
-    return APIInstance.get('db/orderStatus');
-  },
-  postOrder(requestBody) {
-    return APIInstance.post('db/order', requestBody);
-  },
-  getOrder(id) {
-    return APIInstance.get(`db/order/${id}`);
-  },
-  updateOrder(id, requestBody) {
-    return APIInstance.put(`db/order/${id}`, requestBody);
-  },
+  // getPoint() {
+  //   return APIInstance.get('db/point/');
+  // },
+  // getCars() {
+  //   return APIInstance.get('db/car/');
+  // },
+  // getCategories() {
+  //   return APIInstance.get('db/category/');
+  // },
+  // getRates() {
+  //   return APIInstance.get('db/rate');
+  // },
+  // getStatuses() {
+  //   return APIInstance.get('db/orderStatus');
+  // },
+  // postOrder(requestBody) {
+  //   return APIInstance.post('db/order', requestBody);
+  // },
+  // getOrder(id) {
+  //   return APIInstance.get(`db/order/${id}`);
+  // },
+  // updateOrder(id, requestBody) {
+  //   return APIInstance.put(`db/order/${id}`, requestBody);
+  // },
 };
 
-export { authLogin, orderApi };
+export { authLogin, api, URL };
