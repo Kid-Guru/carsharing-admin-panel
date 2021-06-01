ad1fe985291dcbc0e3428e99a7366a20e7468b21
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-01 16:33:18 +0400

    delete unecessary files

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import thunk from 'redux-thunk';
 import App from './App';
 import './index.scss';
 import rootReducer from './redux/rootReducer';
-import reportWebVitals from './reportWebVitals';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
@@ -22,8 +21,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
