a30f500d573dbefb592fe5bd711113656e82660d
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-01 16:11:44 +0400

    change names filter fields

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -38,9 +38,10 @@ function Orders() {
     >
       <Form className={s.filters} action="submit">
         <div className={s.filters__leftCol}>
-          <Field name="example" component={SelectFilter} options={options} placeholder="fregrt" />
-          <Field name="example2" component={SelectFilter} options={options} placeholder="fregrt" />
-          <Field name="example3" component={SelectFilter} options={options} placeholder="fregrt" />
+          <Field name="example" component={SelectFilter} options={options} placeholder="Период" />
+          <Field name="example2" component={SelectFilter} options={options} placeholder="Модель" />
+          <Field name="example3" component={SelectFilter} options={options} placeholder="Город" />
+          <Field name="example3" component={SelectFilter} options={options} placeholder="Статус" />
         </div>
         <div className={s.filters__rightCol}>
           <ButtonSubmit text="Применить" />
