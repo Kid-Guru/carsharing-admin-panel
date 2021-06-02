d964ac1e64dca27f1f2944949348db65ce066ba0
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-02 22:30:32 +0400

    apply filter

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,8 +1,14 @@
 import { Field, Form, Formik } from 'formik';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getOrdersRequest, setPageOrders } from '../../redux/orders/actions';
-import { ordersSelector, totalOrdersSelector } from '../../redux/orders/selectors';
+import { initialOrdersRequest, setFilterOrders, setPageOrders } from '../../redux/orders/actions';
+import {
+  cityFilterSelector,
+  modelFilterSelector,
+  ordersSelector,
+  statusFilterSelector,
+  totalOrdersSelector,
+} from '../../redux/orders/selectors';
 import ButtonSubmit from '../common/Buttons/ButtonSubmit/ButtonSubmit';
 import ListContentLayout from '../common/ListContentLayout/ListContentLayout';
 import OrdersList from '../common/OrdersList/OrdersList';
@@ -10,38 +16,34 @@ import Paginator from '../common/Paginator/Paginator';
 import SelectFilter from '../common/SelectFilterField/SelectFilterField';
 import s from './Orders.module.scss';
 
-const options = [{ label: 'option1', value: 'option1id' }, { label: 'option2', value: 'option2id' }];
-
 function Orders() {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getOrdersRequest());
-    // return () => {
-    //   cleanup
-    // }
-  }, []);
+  useEffect(() => dispatch(initialOrdersRequest()), []);
   // const isFetching = useSelector((state) => state.orders.statuts === 'fetching');
   const ordersList = useSelector(ordersSelector);
   const total = useSelector(totalOrdersSelector);
 
+  const modelOption = useSelector(modelFilterSelector);
+  const cityOption = useSelector(cityFilterSelector);
+  const statusOption = useSelector(statusFilterSelector);
+
   const onPageChange = ({ selected }) => dispatch(setPageOrders(selected + 1));
-  const onSubmitHandle = (e) => console.log(e);
+  const onSubmitHandle = (data) => dispatch(setFilterOrders(data));
   const Filters = () => (
     <Formik
       initialValues={{
-        example: '',
-        example2: '',
-        example3: '',
+        model: null,
+        city: null,
+        status: null,
       }}
       onSubmit={onSubmitHandle}
     >
       <Form className={s.filters} action="submit">
         <div className={s.filters__leftCol}>
-          <Field name="example" component={SelectFilter} options={options} placeholder="Период" />
-          <Field name="example2" component={SelectFilter} options={options} placeholder="Модель" />
-          <Field name="example3" component={SelectFilter} options={options} placeholder="Город" />
-          <Field name="example3" component={SelectFilter} options={options} placeholder="Статус" />
+          <Field name="model" component={SelectFilter} options={modelOption} placeholder="Модель" />
+          <Field name="city" component={SelectFilter} options={cityOption} placeholder="Город" />
+          <Field name="status" component={SelectFilter} options={statusOption} placeholder="Статус" />
         </div>
         <div className={s.filters__rightCol}>
           <ButtonSubmit text="Применить" />
