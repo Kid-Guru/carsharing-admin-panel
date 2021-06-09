18b7c3b719f09f02f050556d9209800bc468a8a7
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-09 11:16:53 +0400

    refactor CheckboxBtnGroup

--- src/components/Form/CheckboxBtnGroup/CheckboxBtnGroup.jsx
+++ src/components/Form/CheckboxBtnGroup/CheckboxBtnGroup.jsx
@@ -1,3 +1,4 @@
+import { Field } from 'formik';
 import s from './CheckboxBtnGroup.module.scss';
 
 function CheckboxBtnGroup(props) {
@@ -8,8 +9,8 @@ function CheckboxBtnGroup(props) {
       <div className={s.checkbox__container}>
 
         {items.map((elem) => (
-          <label key={elem.id} className={s.checkbox__label} htmlFor={elem.text}>
-            <input className={s.checkbox__input} name={title} id={elem.text} type="checkbox" />
+          <label key={elem.text} className={s.checkbox__label} htmlFor={elem.text}>
+            <Field name={elem.name} className={s.checkbox__input} id={elem.text} type="checkbox" />
             <span className={s.checkbox__icon} role="presentation" />
             <span className={s.checkbox__text}>{elem.text}</span>
           </label>
