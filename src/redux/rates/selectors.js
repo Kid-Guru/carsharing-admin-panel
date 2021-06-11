b3646a6fba9915a9d6b81815d6708d4cc94337ba
Kid-Guru <birkov.alexandr@gmail.com>
2021-06-11 18:30:49 +0400

    add fix rateTypeId

--- src/redux/rates/selectors.js
+++ src/redux/rates/selectors.js
@@ -2,7 +2,7 @@ const rateByIdSelector = (state, id) => state.rates.data.find((r) => r.id === id
 
 const rateOptionsSelector = (state) => {
   const { data } = state.rates;
-  return data.map((r) => ({ label: r.rateTypeId.name, value: r.id }));
+  return data.map((r) => ({ label: r.rateTypeId?.name, value: r.id }));
 };
 
 const isFetchingSelector = (state) => state.points.status === 'fetching';
