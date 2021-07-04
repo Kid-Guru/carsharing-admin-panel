1cc116ea43a1bd4401f45837d7c875a10ca44701
Kid-Guru <birkov.alexandr@gmail.com>
2021-07-04 16:55:59 +0400

    fix some bugs

--- src/components/common/ImageCar/ImageCar.jsx
+++ src/components/common/ImageCar/ImageCar.jsx
@@ -1,8 +1,8 @@
-import s from 'react';
+import s from './ImageCar.module.scss';
 
 function ImageCar({ imagePath }) {
   return (
-    <img className={s.image} src={imagePath} alt="Car preview" />
+    <img className={s.image} src={imagePath} alt="Car preview" width="1280" heigth="720" />
   );
 }
 
